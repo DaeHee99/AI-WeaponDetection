79b9e5b62d26ca8ba0e847fa54a91f181bbbadfe
Daehee <mok9550@ajou.ac.kr>
2023-10-17 20:15:17 +0900

    feat: 물체 탐지 종료 기능

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,6 +5,15 @@ function Header() {
     <header className="fixed w-full z-40 top-0 left-0 bg-white border border-b-blue-300 shadow p-4 flex justify-center items-center gap-5">
       <img src={knifeMan} alt="logo" className="w-10 h-10" />
       <div className="text-center text-2xl">위험 물체 탐지 프로그램</div>
+
+      <button
+        className="fixed top-4 right-10 px-6 py-2 leading-6 text-sm shadow rounded-md text-red-500 bg-white ring-1 ring-red-500 hover:bg-red-400 hover:text-white"
+        onClick={() => {
+          location.reload();
+        }}
+      >
+        종료
+      </button>
     </header>
   );
 }
